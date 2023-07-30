0e4ae0b210960277cbda79dfa6912f92ab2664e3
Risyad Rais <satmo54@gmail.com>
2023-07-30 15:48:13 +0700

    Added htmx and some formatting for code legibility

--- resources/views/votes/create.blade.php
+++ resources/views/votes/create.blade.php
@@ -1,15 +1,25 @@
 <!doctype html>
 <head>
-    <link rel="stylesheet" type="text/css" href="{{ asset('css/app.css') }}" />
+    <link
+        rel="stylesheet"
+        type="text/css"
+        href="{{ asset('css/app.css') }}"
+        />
 </head>
 <body>
 
-    <form autocomplete="off">
+    <form 
+        action="/votes"
+        method="POST"
+        autocomplete="off">
+        @csrf
+        <input type="submit" value="Submit" />
         <div class="candidate-list" >
-           @include('votes.candidate-card')
+            @include('votes.candidate-card')
         </div> <!-- end of candidate-list -->
     </form>
     <script type="text/hyperscript" src="{{ asset('js/app._hs') }}"></script>
     <script src="{{ asset('js/_hyperscript.min.js') }}"></script>
+    <script src="{{ asset('js/htmx.min.js') }}"></script>
 </body>
 </html>
